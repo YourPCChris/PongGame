4488c1754eead5f1ce8c86e919165b16f27bea69
ChristopherBennett <chrisjwbennett@gmail.com>
2024-07-27 01:34:57 +0100

    Adding Basic Window using raylib

--- Code/Pong.cpp
+++ Code/Pong.cpp
@@ -4,7 +4,8 @@
 */
 
 
-#include <iostream>
+#include "raylib.h"
+
 
 using namespace std;
 
@@ -12,4 +13,18 @@ using namespace std;
 int main()
 {
 	//cout << "We Ball\n";
+	
+	InitWindow(1000, 800, "Pong");
+	SetWindowState(FLAG_VSYNC_HINT);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			ClearBackground(BLACK);
+			DrawFPS(10,10);
+		EndDrawing();
+	}
+	CloseWindow();
+
+	return 0;
 }
